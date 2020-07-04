15646115ffc155c09dd5c8677f409f45500ff997
trex22 <contact@jasonchalom.com>
2020-07-04 15:50:42 +0200

    Init config :hammer:

--- test/maseti_test.rb
+++ test/maseti_test.rb
@@ -4,8 +4,4 @@ class MasetiTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Maseti::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
