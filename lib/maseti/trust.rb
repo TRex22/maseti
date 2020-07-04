45a3b3db71fbb4cd7cea12bde424dbdeed400591
trex22 <contact@jasonchalom.com>
2020-07-04 19:16:43 +0200

    Bump version :hammer:

---
+++ lib/maseti/trust.rb
@@ -0,0 +1,13 @@
+module Maseti
+  class Trust
+    attr_reader :path
+
+    def initialize(path:)
+      @path = path
+    end
+
+    private
+
+
+  end
+end
--- lib/maseti/version.rb
+++ lib/maseti/version.rb
@@ -1,3 +1,3 @@
 module Maseti
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
